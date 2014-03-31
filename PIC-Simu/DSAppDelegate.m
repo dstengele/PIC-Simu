b3c419115376376995b3a3fbc8aec86102b1618b
Dennis Stengele <d.stengele@derintendant.de>
2014-03-31 14:25:29 +0200

    Initial Commit

---
+++ PIC-Simu/DSAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  DSAppDelegate.m
+//  PIC-Simu
+//
+//  Created by Dennis Stengele on 31/03/14.
+//  Copyright (c) 2014 Dennis Stengele. All rights reserved.
+//
+
+#import "DSAppDelegate.h"
+
+@implementation DSAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
